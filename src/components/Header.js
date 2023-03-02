f015085684555bbeba0ff286b33612109ca5ad84
Cali Huddleston <calihuddleston@gmail.com>
2023-03-02 12:01:10 -0500

    adding files to repo

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+export default function Header () {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a>Home</a>
+                        <a>About</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <h1>KENZIE MARIE PHOTOGRAPHY LLC</h1>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a>Testimonials</a>
+                        <a>Portfolio</a>
+                        <a>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    ) 
+}
