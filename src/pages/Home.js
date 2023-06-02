bf6aa703f104e9c36a429437a276551cdc7c0adb
Grady Peck <gradypeck717@gmail.com>
2023-06-01 21:35:49 -0400

    adjusted indents on Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,11 +11,11 @@ export default function Home () {
     return (
         <main>
             <header>
-            <div>
+                <div>
                     <p className="quote">"A GOOD SNAPSHOT KEEPS</p>
                     <p className="quote">A MOMENT FROM RUNNING AWAY"</p>
                     <div id="box">
-                    <div id="border-bottom"></div>
+                        <div id="border-bottom"></div>
                     </div>
                     <p className="quote title">Family, Portrait, & Wedding Photography</p>
                 </div>
@@ -28,13 +28,13 @@ export default function Home () {
                     <img src={captureImg} height="300px"></img>
                     <p className="capture-para">LIFE IS SHORT </p>
                     <span><GiFlowerPot />Capture <GiFlowerPot /></span> 
-                       <p className="capture-para"> EVERY MOMENT</p>
+                    <p className="capture-para"> EVERY MOMENT</p>
                 </div>
             </section>
 
             <section id="section-two">
                 <div id="memories">
-                <p>The thing I like about photographs, is the memories they hold. <BsSuitHeart /></p>
+                    <p>The thing I like about photographs, is the memories they hold. <BsSuitHeart /></p>
                 </div>
                 <Slideshow />
             </section>
@@ -47,13 +47,12 @@ export default function Home () {
             {/* <div id="background-white"></div> */}
                 <div>
                     <img src={shoot} height="900px"></img>
-                    
                 </div>
                 <div id="about-section">
                     <h2 id="welcome">WELCOME, MY NAME IS <span>Mackenzie</span></h2>
                     <p id="description">I'm a self taught photographer. Learning and growing every second I possibly can! Want to learn more and hear about my story click the button below and I'll run you down memory lane!</p>
                     <div>
-                    <button id="morebtn"><Link to="/about" element={<About />} className="page-links">MORE ABOUT ME</Link></button>
+                        <button id="morebtn"><Link to="/about" element={<About />} className="page-links">MORE ABOUT ME</Link></button>
                     </div>
                 </div>
             </section>
@@ -73,12 +72,11 @@ export default function Home () {
             <section id="section-four">
                 <div id="contact-section">
                     <p id="adventure">ARE YOU READY FOR YOUR NEXT ADVENTURE?</p>
-            
                     <button id="contactbtn"><Link to="/contact" element={<Contact />} className="page-links">CONTACT ME</Link></button>
-                  
                 </div>
-                    <img src={contactImg} height="700px"></img>
+                <img src={contactImg} height="700px"></img>
             </section>
+
         </main>
     )
 }
